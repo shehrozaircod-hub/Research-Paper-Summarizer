b373c15a62cfe7f6437058b11e97ec1567c50b40
shehroz.mir <shehroz.mir@aircod.com>
2025-11-01 23:50:19 +0500

    Created a chatbot

---
+++ prompt_ui_3.py
@@ -0,0 +1,38 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.prompts import PromptTemplate, load_prompt
+from dotenv import load_dotenv
+import streamlit as st
+import os
+
+load_dotenv()
+
+model = ChatOpenAI(model='gpt-4o', max_tokens=2000)
+
+st.header('Research Tool')
+
+paper_input = st.selectbox("Select Research Paper Name", ["Attention Is All You Need", 
+                                                          "BERT: Pre-training of Deep Bidirectional Transformers", 
+                                                          "GPT-3: Language Models are Few-Shot Learners", 
+                                                          "Diffusion Models Beat GANs on Image Synthesis"])
+
+style_input = st.selectbox("Select Summary Style", ["Beginner_Friendly",
+                                                     "Technical",
+                                                     "Code_Oriented",
+                                                     "Mathematical"])
+
+length_input = st.selectbox("Select Summary Length", ["Short (1-2 paragraphs)",
+                                                       "Medium (3-5 paragraphs)",
+                                                       "Long (detailed_explanation)"])
+
+# Template
+
+template = load_prompt('template.json')
+
+if st.button('Summarize'):
+    chain = template | model
+    result = chain.invoke({
+    'a':paper_input,
+    'b':style_input,
+    'c':length_input
+})
+    st.write(result.content)
