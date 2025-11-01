4e126a18e08609e577ac938ac3b5862aac6ee450
shehroz.mir <shehroz.mir@aircod.com>
2025-11-01 23:36:40 +0500

    Implemented prompt genertor

--- prompt_ui_2.py
+++ prompt_ui_2.py
@@ -1,5 +1,5 @@
 from langchain_openai import ChatOpenAI
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate, load_prompt
 from dotenv import load_dotenv
 import streamlit as st
 import os
@@ -26,20 +26,7 @@ length_input = st.selectbox("Select Summary Length", ["Short (1-2 paragraphs)",
 
 # Template
 
-template = PromptTemplate(
-    template = """ Please summarize the research paper titled "{a}" with the following specifications:
-Explanation Style: {b}
-Explanation Length: {c}
-1. Mathematical Details:
--Include relevant mathematical equations if present in the paper.
--Explain the mathematical concepts using simple, intuitive code snippets where applicable.
-2. Analogies:
--Use relatable analogies to simplify complex ideas.
-If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.
-Ensure the summary is clear, accurate, and aligned with the provided style and length.
-""",
-input_variables=['a','b','c']
-)
+template = load_prompt('template.json')
 
 #fill the placeholders
 prompt = template.invoke({
